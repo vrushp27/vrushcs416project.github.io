7f5f6c9140a98bbbde6383d0da797bbddb9fe08a
vrushp27 <77703818+vrushp27@users.noreply.github.com>
2022-07-27 20:43:06 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//FIRST GRAPH
+
 var first_graph = d3.select('#first_graph')
 var keys_cyls = ["2", "4", "6", "8", "10", "12"]
 var width = 920, height = 920
@@ -132,6 +134,7 @@ async function load1() {
     })
 }
 
+//SECOND GRAPH
 
 var second_graph = d3.select('#second_graph')
 
@@ -144,7 +147,7 @@ second_graph.append('text')
     .attr('x', 500)
     .attr('y', 390)
     .attr('text-anchor', 'middle')
-    .text('Average Highway MPG')
+    .text('Average City MPG')
 
 
 second_graph.selectAll("legend")
@@ -225,4 +228,202 @@ async function load2() {
                     .style("opacity", 0);
             });
     })
+}
+
+//THIRD GRAPH
+
+var third_graph = d3.select('#third_graph')
+
+third_graph.append('text')
+    .attr('x', -500)
+    .attr('y', 15)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Number of Cylinders')
+
+third_graph.append('text')
+    .attr('x', 500)
+    .attr('y', 1150)
+    .attr('text-anchor', 'middle')
+    .text('Fuel Type')
+
+
+var keys_fuel = ["Diesel", "Gasoline", "Electricity"]
+var shape = d3.scaleOrdinal()
+    .domain(keys_fuel)
+    .range([d3.symbol().type("circle"), d3.symbol().type("diamond"), d3.symbol().type("square")]);
+
+var scatter_tooltip = d3.select("body")
+    .append("div")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border-radius", "5px")
+    .style("padding", "15px")
+    .style("color", "white")
+
+async function load3() {
+    d3.csv("https://flunky.github.io/cars2017.csv").then(function (data) {
+
+        var fuelScale = d3.scaleBand()
+            .range([0, width])
+            .domain(data.map(function (d) { return d.Fuel; }))
+
+        var fuelAxis = d3.axisBottom()
+            .scale(fuelScale)
+            .ticks(5);
+
+        var cylScale = d3.scaleBand()
+            .range([height, 0])
+            .domain(keys_cyls)
+
+        var cylAxis = d3.axisLeft()
+            .scale(cylScale);
+
+        third_graph.append("g")
+            .attr("transform", "translate(50,160)")
+            .attr("class", "axis")
+            .call(cylAxis);
+
+        third_graph.append("g")
+            .attr("transform", "translate(50,1100)")
+            .attr("class", "axis")
+            .call(fuelAxis)
+            .selectAll("text")
+
+        third_graph.append('g')
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return 200 + fuelScale(d.Fuel); })
+            // .attr("cy", function (d) { return 1050 - cylScale(12-d.EngineCylinders); })
+            .attr("cy", function (d) { return 1000 - 60 * d.EngineCylinders; })
+
+            .attr("r", function (d) { return d.AverageHighwayMPG / 1.5; })
+            .style("fill", function (d) { return "#5E4FA2"; })
+            .style("opacity", "0.1")
+            .attr("stroke", "black")
+            .on("mouseover", function (d) {
+                scatter_tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                scatter_tooltip.html(d.Make)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                scatter_tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    })
+}
+
+//FOURTH GRAPH
+
+var fourth_graph = d3.select('#fourth_graph')
+
+fourth_graph.append("g")
+    .attr("transform", "translate(50,20)")
+    .attr("class", "axis")
+    .call(yAxis);
+
+fourth_graph.append('text')
+    .attr('x', -500)
+    .attr('y', 15)
+    .attr('transform', 'rotate(-90)')
+    .attr('text-anchor', 'middle')
+    .text('Mileage')
+
+fourth_graph.append('text')
+    .attr('x', 500)
+    .attr('y', 1050)
+    .attr('text-anchor', 'middle')
+    .text('Cars')
+
+var makes = ["Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", "Cadillac", "Chevrolet", "Chrysler",
+    "Dodge", "Ferrari", "Fiat", "Ford", "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", "Lamborghini",
+    "Land Rover", "Lexus", "Lincoln", "Lotus", "Maserati", "Mazda", "McLaren Automotive", "Mercedes-Benz", "MINI", "Mitsubishi",
+    "Nissan", "Porsche", "Ram", "Rolls-Royce", "Roush Performance", "smart", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo"];
+
+var highway_mpgs = ["35", "33", "19", "30", "22", "41", "27", "30", "29", "25", "24", "22", "103", "96", "24", "29", "38", "122",
+    "30", "39", "27", "92", "21", "28", "30", "29", "24", "24", "34", "23", "82", "33", "102", "101", "27", "21", "19", "23", "39", "27",
+    "98", "30", "28", "29"];
+
+var city_mpgs = ["25", "24", "12", "23", "13", "30", "20", "22", "21", "16", "15", "16", "121", "118", "17", "21", "30", "150", "22",
+    "30", "19", "120", "14", "22", "22", "23", "17", "16", "26", "16", "85", "24", "121", "124", "21", "14", "12", "14", "32", "21", "92",
+    "23", "21", "22"];
+
+var bar_tooltip = d3.select("body")
+    .append("div")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "black")
+    .style("border-radius", "5px")
+    .style("padding", "15px")
+    .style("color", "white")
+
+async function load4() {
+    d3.csv("https://flunky.github.io/cars2017.csv").then(function (data_given) {
+
+        var makeScale = d3.scaleBand()
+            .range([0, width])
+            .domain(data_given.map(function (d) { return d.Make; }))
+
+        var makeAxis = d3.axisBottom()
+            .scale(makeScale)
+            .ticks(5);
+
+        fourth_graph.append("g")
+            .attr("transform", "translate(50,950)")
+            .attr("class", "axis")
+            .call(makeAxis)
+            .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-30)")
+            .style("text-anchor", "end");
+
+        fourth_graph.selectAll("mybar")
+            .data(data_given)
+            .enter()
+            .append("rect")
+            .attr("x", function (d, i) { return margin.left + makeScale(makes[i]); })
+            .attr("y", function (d, i) { return y(highway_mpgs[i]) + 10; })
+            .attr("width", makeScale.bandwidth() - 10)
+            .attr("height", function (d, i) { return height - y(highway_mpgs[i]); })
+            .attr("fill", "#5E4FA2").on("mouseover", function (d, i) {
+                bar_tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                bar_tooltip.html(makes[i])
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function (d) {
+                bar_tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    })
+}
+
+// This function is called by the buttons on top of the plot
+function change(setting) {
+    if (setting === "AverageHighwayMPG") {
+        fourth_graph.selectAll("rect")
+            .transition()
+            .duration(2000)
+            .attr("fill", "#5E4FA2")
+            .attr("y", function (d, i) { return y(highway_mpgs[i]) + 10; })
+            .attr("height", function (d, i) { return height - y(highway_mpgs[i]); })
+    } else {
+        fourth_graph.selectAll("rect")
+            .transition()
+            .duration(2000)
+            .attr("fill", "#66C2A5")
+            .attr("y", function (d, i) { return y(city_mpgs[i]) + 10; })
+            .attr("height", function (d, i) { return height - y(city_mpgs[i]); })
+    }
 }
